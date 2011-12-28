8bfc5e96e2c929954251b32698782a99cd1daed5
Edgar Roman <edgar@edgarroman.com>
2011-12-28 07:04:50 -0800

    added images, js, and styles

---
+++ photo/static/ocean/js/clear_inputs.js
@@ -0,0 +1,15 @@
+﻿function doClear(theText) { if (theText.value == theText.defaultValue) { theText.value = "" } }
+function doDefault(theText) { if (theText.value == "") { theText.value = theText.defaultValue } }
+
+ $(document).ready(function(){
+	   $('.animate img').hover(
+		function(){
+			var $this = $(this);
+				$this.stop().animate({'opacity':'0.7'});
+			},
+			function(){
+				var $this = $(this);
+					$this.stop().animate({'opacity':'1.0'});
+				}
+			);				
+	});
